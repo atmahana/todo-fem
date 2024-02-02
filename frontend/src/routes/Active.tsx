21b9996125e095d9160661f0e04220645686fb52
Adham <zubairadham.ar@gmail.com>
2024-02-02 16:01:44 +0800

    Add conditional rendering to pages

--- frontend/src/routes/Active.tsx
+++ frontend/src/routes/Active.tsx
@@ -1,10 +1,26 @@
 import { FC } from "react";
-import { Content } from "../components/Content";
+import { useAuth } from "@clerk/clerk-react";
+import Loader from "../components/Loader";
+import Content from "../components/Content";
 
 interface ActivePageProps {}
 
-const ActivePage: FC<ActivePageProps> = () => (
-  <Content type="active"/>
-);
+const ActivePage: FC<ActivePageProps> = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <Loader />;
+  }
+
+  if (!isSignedIn) {
+    return (
+      <div className="bg-foreground grid divide-y-2 rounded-md p-5 text-sm text-muted shadow-sm">
+        No task(s) available
+      </div>
+    );
+  }
+
+  return <Content type="active" />;
+};
 
 export default ActivePage;
--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,10 +1,26 @@
 import { FC } from "react";
-import { Content } from "../components/Content";
+import { useAuth } from "@clerk/clerk-react";
+import Loader from "../components/Loader";
+import Content from "../components/Content";
 
 interface HomePageProps {}
 
-const HomePage: FC<HomePageProps> = () => (
-  <Content type="all" />
-);
+const HomePage: FC<HomePageProps> = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <Loader />;
+  }
+
+  if (!isSignedIn) {
+    return (
+      <div className="bg-foreground grid divide-y-2 rounded-md p-5 text-sm text-muted shadow-sm">
+        No task(s) available
+      </div>
+    );
+  }
+
+  return <Content type="all" />;
+};
 
 export default HomePage;
